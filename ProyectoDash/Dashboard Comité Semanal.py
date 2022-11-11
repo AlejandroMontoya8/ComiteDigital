b7fd1f48fed1eaa2dd88a0421270041e3e289fcf
AlejandroMontoya8 <110314404+AlejandroMontoya8@users.noreply.github.com>
2022-11-11 17:35:00 -0500

    Actualización
    ok

--- ProyectoDash/Dashboard Comité Semanal.py
+++ ProyectoDash/Dashboard Comité Semanal.py
@@ -59,7 +59,7 @@ resumen_total["Anio_Orden"] = Anio_Orden
 
 
 #meses = [1,2,3,4,5,6,7,8,9,10]
-semanas = [34,35,36,37,38,39,40,41]
+semanas = [40,41,42,43,44,45]
 
 #resumen_total = resumen_total[resumen_total.Mes_Orden.isin(meses)]
 resumen_total = resumen_total[resumen_total.Semana_Orden.isin(semanas)]
